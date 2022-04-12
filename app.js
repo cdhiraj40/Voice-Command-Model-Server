d3b5ca28b39c95157c87808db158fbd32091a716
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-04-12 17:58:03 +0530

    resolved bug

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
+const Joi = require('@hapi/joi');
+const bodyParser  = require('body-parser');
 
+const modelVersions = require( './model_versions.json')
 
-import model_versions from './model_versions.json'
-const bodyParser = require('body-parser');
-
-const modelVersions = model_versions;
-
-/** Takes care of JSON data */
 app.use(express.json());
-
-/** Parse the request */
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.get('/', (res) => {
-    res.send('EnviroCar Model!!');
+app.get('/', (req,res) => {
+    res.send('Envirocar Model Backend!!');
+})
+
+app.get('/test', (req, res) =>{
+    console.log(req.body)
+    res.send(req.body)
 })
 
 app.get('/model', (versionCode, res) => {
-    console.log(versionCode.body);
-
+    console.log(versionCode.body);  
     // get the model link
     var modelLink = getModelByVersion(modelVersions, versionCode);
     res.send(modelLink);
